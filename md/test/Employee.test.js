a5aae52f854e39c3d62fa4caf5a5205bedd6190d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-17 11:56:45 -0400

    Employe test

--- md/test/Employee.test.js
+++ md/test/Employee.test.js
@@ -1,28 +1,52 @@
 /*Employee Tests */
-//*EX: Unit 10 ex13-16
-/*describe('Todo', () => {
-  describe('Initialization', () => {
-    // Positive test
-    it("should create an object with a 'text' property set to the 'text' argument provided when called with the 'new' keyword", () => {
-      // Arrange
-      const text = 'Pick up milk';
-      // Act
-      const obj = new Todo(text);
-      // Assert
-      expect(obj.text).toEqual(text);
-    });
-    // Exception test
-    it("should throw an error if not provided a 'text' value", () => {
-      // Arrange
-      const cb = () => new Todo();
-      const err = new Error(
-        "Expected parameter 'text' to be a non empty string"
-      );
-       // Assert
-      expect(cb).toThrowError(err);
-    });
-  });
+
+const { TestWatcher } = require("jest");
+const Employee = require("../lib/Employee");
+
+TestWatcher("Can instantiate Employee insatnce", () => {
+  const e = new Employee ();
+expect(typeof(e)).tobe("object");
 });
 
-*/
+TestWatcher("Can set name via constructor", () => {
+  const name = Brendan
+  const e = new Employee(name);
+  expect(e.name).tobe(name);
+});
+
+TestWatcher("Can set ID via constructor", () => {
+  const testvalue = 100;
+  const e = new Employee("Foo", testvalue);
+  expect(e.id).tobe(testvalue);
+});
+
+TestWatcher("Can set email via constructor", () => {
+  const testvalue = "email@email.com";
+  const e = new Employee("Foo", 1, testvalue);
+  expect(e.email).tobe(testvalue);
+});
+
+TestWatcher("Can get name via getName()", () => {
+  const testvalue = "Brendan";
+  const e = new Employee(testvalue);
+  expect(e.getName()).tobe(testvalue);
+});
+
+TestWatcher("Can get ID  via getId()", () => {
+  const testvalue = 100;
+  const e = new Employee("Foo", testvalue);
+  expect(e.getId()).tobe(testvalue);
+});
+
+TestWatcher("Can get email via getEmail()", () => {
+  const testvalue = "email@email.com";
+  const e = new Employee("Foo", 1, testvalue);
+  expect(e.getEmail()).tobe(testvalue);
+});
+
+TestWatcher("getRole() should return \"Employee\"", () => {
+  const testvalue = "Employee";
+  const e = new Employee("Brendan", 1, testvalue);
+  expect(e.getRole()).tobe(testvalue);
+});
 
